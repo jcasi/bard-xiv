787e8668434d1ba01bfcad6143d7580fc7dd6ba3
James Casillas <jamesccasillas@gmail.com>
2023-06-10 13:44:10 -0700

    started search/results component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,21 +1,18 @@
 import React from "react";
-import Image from "next/image";
 
 type Props = {};
 
 export default function Sidebar({}: Props) {
   return (
-    <div className="container fixed left-0 top-0 w-[12%] h-full border-r border-r-raisin bg-gunmetal">
-      <p className="inline-flex p-2 items-center ">
-        <img
-          className="h-12 w-12 my-auto"
-          src="/bard-flat.png"
-          alt="bard icon"
-        />
-        <h1 className="text-4xl text-flax font-trajan pl-2">Bard XIV</h1>
+    <div className="fixed left-0 top-0 w-[12%] h-full border-r-2 border-r-raisin bg-gunmetal">
+      <p className="inline-flex pl-2 pt-4 pb-4 items-center border-b-2 border-b-raisin">
+        <img className="w-[20%] my-auto" src="/bard-flat.png" alt="bard icon" />
+        <h1 className="text-3xl text-flax font-trajan pl-2 pt-2">Bard XIV</h1>
       </p>
       <ul className="pt-4">
-        <li className="p-4">Bard</li>
+        <li className="pl-4 pt-3">Bard</li>
+        <li className="pl-4 pt-3">Bard</li>
+        <li className="pl-4 pt-3">Bard</li>
       </ul>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Sidebar from "@/components/Sidebar";
+import Results from "@/components/Results";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function Home() {
       </Head>
 
       <Sidebar />
+      <Results />
     </div>
   );
 }
