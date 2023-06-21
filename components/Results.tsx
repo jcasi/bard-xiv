74046860bb308d0e2c749bbdaab95111000b1451
jccasil <jamesccasillas@gmail.com>
2023-06-20 22:36:23 -0700

    Implemented search parameter for API call

--- components/Results.tsx
+++ components/Results.tsx
@@ -3,19 +3,28 @@ import React from "react";
 type Props = {};
 
 export default function Results({}: Props) {
-  const results = await fetch(
-    "https://xivapi.com/search?string=${searchString}",
-    {
-      method: "GET",
-      headers: {
-        Accept: "applications/json",
-        "Content-Type": "applications/json",
-      },
-    }
-  );
+  var searchParam = "rainbow";
+  var count = 0;
+  var parsed;
+
+  const appendCall = "https://xivapi.com/search?string=" + searchParam;
+
+  fetch(appendCall)
+    .then((response) => response.json())
+    .then((response) => {
+      parsed = response.Results;
+      for (var i = 0; i < parsed.length; i++) {
+        var result = parsed[i];
+        if (result.UrlType === "Item") {
+          console.log(result);
+          count++;
+        }
+      }
+      //console.log(count); //for debugging
+    });
 
   return (
-    <div className="absolute left-[12%] w-[40%] bg-gunmetal h-full">
+    <div className="absolute left-[14%] w-[40%] bg-gunmetal h-full">
       <div className="flex pl-2 pt-4 pb-4 items-center border-b-2 border-b-raisin">
         <img
           className="lg:w-10 md:w-8 sm:w-8"
