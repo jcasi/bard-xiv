1c704b36e5c5023ab9fa685fe30f6be8fa180ce7
jccasil <jamesccasillas@gmail.com>
2023-07-07 17:25:08 -0700

    started styling item result info

--- components/Results.tsx
+++ components/Results.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Results({}: Props) {
   const [results, setResults] = useState<any[]>([]);
-  var searchParam = "approved grade 4 skybuilder";
+  var searchParam = "diadochos";
   let parsed = [];
   const itemList = new Array<any>();
   const appendLink = "https://xivapi.com/search?string=" + searchParam;
@@ -15,7 +15,7 @@ export default function Results({}: Props) {
       .then((data) => {
         console.log(data);
         parsed = data.Results;
-        for (var i = 0; i < parsed.length; i++) {
+        for (var i = 0; i < 12; i++) {
           if (parsed[i].UrlType === "Item") {
             //console.log(parsed[i]);
             itemList.push(parsed[i]);
@@ -28,22 +28,24 @@ export default function Results({}: Props) {
 
   return (
     <div className="absolute left-[14%] w-[40%] bg-gunmetal h-full">
-      <div className="flex pl-2 pt-4 pb-4 items-center border-b-2 border-b-raisin">
+      <div className="flex pl-2 pt-5 pb-4 items-center border-b-2 border-b-raisin">
         <img
           className="lg:w-10 md:w-8 sm:w-8"
           src="/bard-magni.png"
           alt="search icon"
         />
-        <h1 className="lg:text-3xl md:text-xl sm:text-sm text-spring pt-2 pl-4">
-          Searchbar to go here
+        <h1 className="lg:text-xl md:text-xl sm:text-sm text-cadet pt-2 pl-4">
+          Search market board
         </h1>
       </div>
-      <div>
+      <div className="">
         {results.map((result) => {
           return (
-            <div>
+            <div className="flex border-b-2 border-b-raisin p-2">
               <img className="lg:w-10 md:w-8 sm:w-8" src={result.Icon} />
-              <h1>{result.Name}</h1>
+              <p>
+                <h1 className="pl-2 text-babypowder">{result.Name}</h1>
+              </p>
             </div>
           );
         })}
